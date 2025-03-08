8e1753b2528dd662ed2a4957695e433b239632ac
cristian <cregueiromartinez@danielcastelao.org>
2025-03-08 12:22:45 +0100

    aplication properties de postgres

--- postgresService/src/main/java/com/example/cristian/postgresService/config/PostgreSQLConfig.java
+++ postgresService/src/main/java/com/example/cristian/postgresService/config/PostgreSQLConfig.java
@@ -9,23 +9,33 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 /**
- *
+ * Clase de configuracion de postgreSQL
+ * @author cristian
+ * @version 1.0
  */
 @Configuration
 public class PostgreSQLConfig {
 
+    //url de la base de datos
     @Value("${spring.datasource.url}")
     private String url;
 
+    //usuario de la base de datos
     @Value("${spring.datasource.username}")
     private String user;
 
+    //contraseña de la base de datos
     @Value("${spring.datasource.password}")
     private String password;
 
+    //driver de la base de datos
     @Value("${spring.datasource.driver-class-name}")
     private String driver;
 
+    /**
+     * Metodo que devuelve la conexion de la base de datos
+     * @return la conexion a la base
+     */
     @Bean
     public DataSource dataSource() {
         try {
--- postgresService/src/main/java/com/example/cristian/postgresService/excepciones/ConnectionDbException.java
+++ postgresService/src/main/java/com/example/cristian/postgresService/excepciones/ConnectionDbException.java
@@ -1,5 +1,10 @@
 package com.example.cristian.postgresService.excepciones;
 
+/**
+ * Excepcion personalizada de fallo de conexion a postgresSQL
+ * @author cristian
+ * @version 1.0
+ */
 public class ConnectionDbException extends RuntimeException {
     public ConnectionDbException(String message) {
         super(message);
