17be46533b41a3156238286e1b1d5b6137489f30
cristian <cregueiromartinez@danielcastelao.org>
2025-03-09 12:23:37 +0100

    rest controller actualizado de album, vamos a probar todo

--- postgresService/src/main/java/com/example/cristian/postgresService/controller/AlbumRestController.java
+++ postgresService/src/main/java/com/example/cristian/postgresService/controller/AlbumRestController.java
@@ -40,5 +40,56 @@ public class AlbumRestController {
         return ResponseEntity.ok().body("Album añadido correctamente");
     }
 
+    /**
+     * Metodo para borrar un album en postgreSQL
+     * @param id el id del album a borrar
+     * @return un mensaje indicando si se borró o no
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteAlbumByIdPostgreSQLController(@PathVariable Integer id) {
+        try{
+            boolean eliminado = albumService.deleteAlbumByIdService(id);
+            if(!eliminado){
+                return ResponseEntity.badRequest().body("Album no encontrado");
+            }
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body("Album eliminado correctamente");
+    }
+
+    /**
+     * Metodo para crear un album en postgreSQL y llamar a mongoService
+     * @param albumAuxDTO el album DTO a crear en postgreSQL y mongoService
+     * @return un mensaje indicando si se creo o no
+     */
+    @PostMapping("/crear")
+    public ResponseEntity<String> createNewAlbumLlamadaPostgreSQLController(@RequestBody AlbumAuxDTO albumAuxDTO) {
+        try{
+            albumService.createAlbumService(albumAuxDTO);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body("Album creado correctamente en llamada");
+    }
+
+    /**
+     * Metodo para borrar un album en postgreSQL y mongoService
+     * @param id el id del album a borrar en ambas bases de datos
+     * @return un mensaje indicando si se borró o no
+     */
+    @DeleteMapping("/borrar/{id}")
+    public ResponseEntity<String> borrarAlbumByIdLlamadaPostgreSQLController(@PathVariable Integer id) {
+        try{
+            boolean eliminado = albumService.borrarAlbumByIdService(id);
+            if(!eliminado){
+                return ResponseEntity.badRequest().body("Album no encontrado");
+            }
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok().body("Album borrado correctamente en llamada");
+    }
+
 
 }
