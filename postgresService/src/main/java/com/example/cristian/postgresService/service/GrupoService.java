639d414d3ca7d0e1d4446067c2124a371a03dfdf
cristian <cregueiromartinez@danielcastelao.org>
2025-03-09 12:44:04 +0100

    modificando servicio llamando a mongoService

--- postgresService/src/main/java/com/example/cristian/postgresService/service/GrupoService.java
+++ postgresService/src/main/java/com/example/cristian/postgresService/service/GrupoService.java
@@ -3,6 +3,7 @@ package com.example.cristian.postgresService.service;
 import com.example.cristian.postgresService.model.dto.GrupoDTO;
 import com.example.cristian.postgresService.model.entity.Grupo;
 import com.example.cristian.postgresService.repository.GrupoRepository;
+import com.example.cristian.postgresService.servicio_comunicacion.ServicioMongo;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,13 +16,15 @@ public class GrupoService {
 
     //variable del repositorio
     private final GrupoRepository grupoRepository;
+    private final ServicioMongo servicioMongo;
 
     /**
      * Constructor de la clase
      * @param grupoRepository el repositorio del grupo
      */
-    public GrupoService(GrupoRepository grupoRepository) {
+    public GrupoService(GrupoRepository grupoRepository, ServicioMongo servicioMongo) {
         this.grupoRepository = grupoRepository;
+        this.servicioMongo = servicioMongo;
     }
 
     /**
@@ -54,6 +57,7 @@ public class GrupoService {
         Grupo grupo = new Grupo(grupoDTO.getNome(), grupoDTO.getXenero(),
                 grupoDTO.getDstaFormacion());
         grupoRepository.save(grupo);
+        servicioMongo.crearGrupoLlamada(grupoDTO);
     }
 
     /**
