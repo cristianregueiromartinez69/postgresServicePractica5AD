d0cada21da65a62f8f84e6d205f161210c4631a5
cristian <cregueiromartinez@danielcastelao.org>
2025-03-10 16:59:35 +0100

    creando metodo de llamada para crear album en mongo

--- postgresService/src/main/java/com/example/cristian/postgresService/servicio_comunicacion/ServicioMongo.java
+++ postgresService/src/main/java/com/example/cristian/postgresService/servicio_comunicacion/ServicioMongo.java
@@ -1,5 +1,6 @@
 package com.example.cristian.postgresService.servicio_comunicacion;
 
+import com.example.cristian.postgresService.model.dto.AlbumAuxMongoServiceDTO;
 import com.example.cristian.postgresService.model.dto.GrupoAuxMongoServiceDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,6 +30,12 @@ public interface ServicioMongo {
     @DeleteMapping("/Practica5MongoService/grupo/borrar/{id}")
     void borrarGrupoLlamada(@PathVariable Integer id);
 
+    /**
+     * Metodo para crear un album llamando a mongoService
+     * @param albumAuxMongoServiceDTO el objeto a crear en mongo
+     */
+    @PostMapping("/Practica5MongoService/album/crear")
+    void crearAlbum(@RequestBody AlbumAuxMongoServiceDTO albumAuxMongoServiceDTO);
 
 
 }
